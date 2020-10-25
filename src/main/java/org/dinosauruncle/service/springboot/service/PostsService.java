b379ed45add8840739115a47aaba1acddf9850d3
박 종훈 <m05214@naver.com>
2020-10-25 23:57:55 +0900

    4. Posts API update & Select 기능 추가

--- src/main/java/org/dinosauruncle/service/springboot/service/PostsService.java
+++ src/main/java/org/dinosauruncle/service/springboot/service/PostsService.java
@@ -1,8 +1,11 @@
 package org.dinosauruncle.service.springboot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.dinosauruncle.service.springboot.domain.posts.Posts;
 import org.dinosauruncle.service.springboot.domain.posts.PostsRepository;
+import org.dinosauruncle.service.springboot.web.dto.PostsResponseDto;
 import org.dinosauruncle.service.springboot.web.dto.PostsSaveRequestDto;
+import org.dinosauruncle.service.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,5 +18,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException
+                        ("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
